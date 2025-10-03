eb394e976ac1be3969ba3953180cb58046b7030d
jasarel <160278797+jasarel@users.noreply.github.com>
2025-10-03 12:35:17 -0500

    Update conftest.py

--- lesson-3-data-validation/exercises/exercise_9/starter/conftest.py
+++ lesson-3-data-validation/exercises/exercise_9/starter/conftest.py
@@ -10,6 +10,8 @@ def pytest_addoption(parser):
     parser.addoption("--reference_artifact", action="store")
     parser.addoption("--sample_artifact", action="store")
 
+    parser.addoption("--ks_alpha", action="store")
+
     # COMPLETE HERE: add the option for ks_alpha
 
 
@@ -37,6 +39,12 @@ def data(request):
 
 @pytest.fixture(scope='session')
 def ks_alpha(request):
+    ks_alpha = request.config.option.ks_alpha
+
+    if ks_alpha is None:
+        pytest.fail("--ks_threshold missing on command line")
+
+    return float(ks_alpha)
 
     # COMPLETE HERE: read the option ks_alpha from the command line,
     # and return it as a float
